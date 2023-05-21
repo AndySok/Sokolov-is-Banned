1d57c2d11fe50051cc9ec3edab3d0ea24cb19adc
AndySok <andrey.sokolov123@gmail.com>
2023-05-21 14:05:47 -0400

    parser parses the entire levels directory

--- bashImplementation/Parser.java
+++ bashImplementation/Parser.java
@@ -1,14 +1,28 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 import java.nio.file.*;
 
 public class Parser {
  public static void main(String[] args){
-  Path filePath = Path.of("./levels/1");
+  List<File> levels = getLevels("./levels");
   try{
-    String content = Files.readString(filePath);
-    System.out.println(content);
+    System.out.println(Files.readString(levels.get(0).toPath()));
+  } catch(Exception e){
+    System.out.println("levels failed to load!");
+  }
+ }
+
+ public static List<File> getLevels(String levelsPath){
+  try{
+    List<File> levels = Files.list(Paths.get(levelsPath))
+      .map(Path::toFile)
+      .filter(File::isFile)
+      .collect(Collectors.toList());
+      return levels;
   } catch (IOException e){
-    System.out.println("file doesn't exist!");
+    e.printStackTrace();
+      return null;
   }
  }
 }
